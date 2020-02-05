a7e3051aaf3ba1207181c834a6e5127af97ad264
Chris Brooks <cbrooks86@gmail.com>
2020-02-05 15:47:13 -0600

    Getting the results to display correctly

--- main.js
+++ main.js
@@ -2,17 +2,29 @@ document.getElementById("get").addEventListener("click", function () {
     var search = document.getElementById("search")["value"];
     var year = document.getElementById("year")["value"];
     if (search.length > 2) {
-        console.log("search");
         fetch("http://www.omdbapi.com/?apikey=22931130&s=" + search + "&y=" + year + "&type=movie")
             .then(function (response) {
-            console.log(response);
             if (!response.ok) {
-                console.log(response.status);
             }
             return response.json();
         })
             .then(function (res) {
-            console.log(res);
+            if (res.Error !== "Movie not found!") {
+                res.Search.forEach(function (element) {
+                    var movieDiv = document.createElement("div");
+                    var movieTitle = document.createElement("h2");
+                    var moviePoster = document.createElement("img");
+                    movieTitle.innerText = element.Title + " | " + element.Year;
+                    moviePoster.src = element.Poster;
+                    document.getElementById("movies").append(movieDiv);
+                    movieDiv.append(movieTitle);
+                    movieDiv.append(moviePoster);
+                    moviePoster.classList.add("movie-poster");
+                });
+            }
+            else {
+                document.getElementById("movies").innerText = "No Movies Found";
+            }
         });
     }
     else {
--- main.ts
+++ main.ts
@@ -2,17 +2,28 @@ document.getElementById("get").addEventListener("click", function () {
     let search: string = document.getElementById("search")["value"]
     let year: number = document.getElementById("year")["value"]
     if (search.length > 2) {
-        console.log("search")
         fetch(`http://www.omdbapi.com/?apikey=22931130&s=${search}&y=${year}&type=movie`)
             .then(function (response) {
-                console.log(response);
                 if (!response.ok) {
-                    console.log(response.status);
                 }
                 return response.json();
             })
             .then(function (res) {
-                console.log(res);
+                if(res.Error !== "Movie not found!"){
+                    res.Search.forEach(element => {
+                        let movieDiv = document.createElement("div");
+                        let movieTitle = document.createElement("h2")
+                        let moviePoster = document.createElement("img")
+                        movieTitle.innerText = `${element.Title} | ${element.Year}`
+                        moviePoster.src = element.Poster;
+                        document.getElementById("movies").append(movieDiv)
+                        movieDiv.append(movieTitle)
+                        movieDiv.append(moviePoster)
+                        moviePoster.classList.add("movie-poster");
+                    });}
+                   else{
+                       document.getElementById("movies").innerText = "No Movies Found"
+                   } 
             })
     } else {
         alert("Your search term must be more than 2 characters");
